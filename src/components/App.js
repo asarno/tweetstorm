8f6b4ebdb161b6b83acc93621674d2efcabf5c65
Anthony Sarno <asarno17@gmail.com>
2017-02-01 15:38:31 -0800

    cats version

--- src/components/App.js
+++ src/components/App.js
@@ -17,13 +17,3 @@ export default App
 
 
 
-
-// const ws = new WebSocket('ws://localhost:3000');
-
-// ws.onopen = () => {
-//   ws.send('location');
-// };
-
-// ws.onmessage = (message) => {
-//   console.log(JSON.parse(message.data).text);
-// };
